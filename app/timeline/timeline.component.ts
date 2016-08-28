cc1e6a6a77d9c3b49bab3eda6be68eb6119f9f47
klbrisson <klbrisson@gmail.com>
2016-08-28 14:23:50 -0600

    add timeline mock data

--- app/timeline/timeline.component.ts
+++ app/timeline/timeline.component.ts
@@ -11,14 +11,24 @@ import { EventService } from './event.service'
 })
 export class TimelineComponent implements OnInit {
     events: any[];
+    selectedId: number;
 
     constructor(private _eventService: EventService) { }
 
     ngOnInit() { 
         this._eventService.getEvents()
-          .then((events) => this.events = events)
+          .then((events) => this.events = events.sort(this.sortByDate))
           .catch((err) => {
             console.log(err);
           });
     }
+
+    selectEvent(id: number) {
+        this.selectedId = id;
+    }
+
+    private sortByDate(eventA: any, eventB: any) {
+        return eventA.date > eventB.date;
+    }
+
 }
