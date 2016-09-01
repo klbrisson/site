0696a3dbd8a198daf77de51317b1cd7ca93cd7ee
klbrisson <klbrisson@gmail.com>
2016-08-31 19:42:32 -0600

    undo testing

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const server            = express();
 const baseDirectoryPath = ".";
 const baseDirectory     = express.static(baseDirectoryPath);
 const indexFile         = path.resolve(`${baseDirectoryPath}/index.html`);
-const mainJS         = path.resolve(`${baseDirectoryPath}/app/main.js`);
 const serverPort        = process.env.PORT || 8080;
 const serveStaticFile   = (_, response) => { response.sendFile(indexFile); }
 const logServerMessage  = () => { console.log(`Express server listening on port ${serverPort}`) }
 
 server.use(baseDirectory);
-server.get('/', serveStaticFile);
-server.get('/app/main.js', mainJS);
+server.get('*', serveStaticFile);
 server.listen(serverPort, logServerMessage);
