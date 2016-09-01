d1f00a7a69eda356706479d72eb8f672ffb7753f
klbrisson <klbrisson@gmail.com>
2016-08-31 19:23:36 -0600

    update server.js

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = require("express");
-const path    = require("path");
+var express = require('express');
+var app = express();
 
-const server            = express();
-const baseDirectoryPath = ".";
-const baseDirectory     = express.static(baseDirectoryPath);
-const indexFile         = path.resolve(`${baseDirectoryPath}/index.html`);
-const serverPort        = process.env.PORT || 8080;
-const serveStaticFile   = (_, response) => { response.sendFile(indexFile); }
-const logServerMessage  = () => { console.log(`Express server listening on port ${serverPort}`) }
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-server.use(baseDirectory);
-server.get('*', serveStaticFile);
-server.listen(serverPort, logServerMessage);
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/app'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // make sure index is in the right directory. In this case /app/index.html
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
