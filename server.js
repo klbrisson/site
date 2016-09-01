c575a8468732438c71bfae4294cddb404e0ca2fa
klbrisson <klbrisson@gmail.com>
2016-08-31 20:07:01 -0600

    test

--- server.js
+++ server.js
@@ -1,67 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require("express");
+const path    = require("path");
 
-var users = require('./routes/users');
-
-var app = express();
-
-if (app.get('env') !== 'production') {
-
-  // expose node_modules to client app
-  app.use(express.static(__dirname + "/node_modules"));
-}
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'app')));
-
-app.get('/', function(req, res, next) {
-  res.json({
-    name: "John",
-    last: "Smith"
-  });
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
-});
+const server            = express();
+const baseDirectoryPath = ".";
+const baseDirectory     = express.static(baseDirectoryPath);
+const indexFile         = path.resolve(`${baseDirectoryPath}/index.html`);
+const serverPort        = process.env.PORT || 8080;
+const serveStaticFile   = (_, response) => { response.sendFile(indexFile); }
+const logServerMessage  = () => { console.log(`Express server listening on port ${serverPort}`) }
 
 
-module.exports = app;
+server.use('/app', express.static(path.resolve(__dirname, 'app')));
+server.use('/libs', express.static(path.resolve(__dirname, 'libs')));
+server.use(baseDirectory);
+server.get('*', serveStaticFile);
+server.listen(serverPort, logServerMessage);
